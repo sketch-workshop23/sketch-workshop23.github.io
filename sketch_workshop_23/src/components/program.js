016753b2bdf1993ecb6694613035ae337753c55f
Josh Cherian <jcherian92@gmail.com>
2022-11-20 16:22:11 -0600

    Added favicon and removed program

--- sketch_workshop_23/src/components/program.js
+++ sketch_workshop_23/src/components/program.js
@@ -1,10 +1,8 @@
 function Program() {
   return (
     <div className="description">
-      <h2>Monday, March 27, 2023 (8:00 AM-12:00 PM AET)</h2>
-      <p>8:00 - 8:30&emsp;&emsp;Opening Remarks (Chair: Paul Taele)</p>
-      <p>8:30 - 9:30&emsp;&emsp;Keynote (Keynote Speaker: )</p>
-      <p>9:30 - 11:00&emsp;&emsp;Presentations</p>
+      <h2>Monday, March 27, 2023</h2>
+      <p>TBD</p>
     </div>
   );
 }
